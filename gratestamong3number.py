bebbce9f1c9816ea795f53cdaf675a3e64471ecd
VENUJAN <VENUJAN@users.noreply.github.com>
2017-07-21 18:23:30 +0530

    Create gratestamong3number.py

---
+++ gratestamong3number.py
@@ -0,0 +1,10 @@
+x = int(input("enter the first number"))
+y = int(input("enter the second number"))
+z = int(input("enter the third number"))
+if (x >= y) and (x >= z):
+largest = x
+ifel ( y >= x) and (y >= z ):
+largest = y
+else
+lagest = z
+print("the largest number between " , x, Y,  "and", z "is" lagest)
